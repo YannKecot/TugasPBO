486bc0691093390a236131497a86b5d2352d1d65
ASUS <ASUS@10.5.13.34>
2024-11-13 13:07:31 +0800

    Tugas1

---
+++ src/main/java/com/mycompany/app3c/App3c.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.app3c;
+
+/**
+ *
+ * @author ASUS
+ */
+public class App3c {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
